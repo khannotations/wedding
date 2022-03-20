5653e102043fd4a7e990a13245577d7f30f464f9
Rafi Khan <rafi.fa.khan@gmail.com>
2022-03-20 21:21:58 +0200

    Meta image

--- javascripts/script.js
+++ javascripts/script.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
   $(window).scroll( function(){
     /* Check the location of each desired element */
     $('.fade-in-scroll').each( function(i){
-      var topObject = $(this).position().top + 200;
+      var topObject = $(this).position().top + 100;
       var bottomWindow = $(window).scrollTop() + $(window).height();
       
       /* If the object is completely visible in the window, fade it it */
       if(bottomWindow > topObject) {
-        $(this).animate({ 'opacity':'1' }, 1500);
+        $(this).animate({ 'opacity':'1' }, 2000);
       }
     });
   });

5cd68a6220990c6aad71d999b9799d13e5950712
Rafi Khan <rafi.fa.khan@gmail.com>
2022-03-20 19:32:42 +0200

    Update with photos.

---
+++ javascripts/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  /* Every time the window is scrolled ... */
+  $(window).scroll( function(){
+    /* Check the location of each desired element */
+    $('.fade-in-scroll').each( function(i){
+      var topObject = $(this).position().top + 200;
+      var bottomWindow = $(window).scrollTop() + $(window).height();
+      
+      /* If the object is completely visible in the window, fade it it */
+      if(bottomWindow > topObject) {
+        $(this).animate({ 'opacity':'1' }, 1500);
+      }
+    });
+  });
+});
